a94264e80569318aa3ae1ed4c1c329193fc5e243
FiboPolo <79830999+FiboPolo@users.noreply.github.com>
2023-04-10 15:03:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.height = 1024
-canvas.width = 576
+canvas.height = 576
+canvas.width = 1024
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
