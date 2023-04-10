ba4cd28a2265943302a341a76835b130907660e4
Steve PB <spoloboyd@gmail.com>
2023-04-10 12:39:05 +0200

    Update

--- index.js
+++ index.js
@@ -7,19 +7,28 @@ canvas.width = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const gravity = 0.2
 
 class Sprite {
   constructor({position, velocity}){ //add more properties to argument and wrap them with{ } to make them pass as properties within an object
     this.position = position
     this.velocity = velocity
+    this.height = 150
   }
   draw(){
     c.fillStyle = 'blue'
-    c.fillRect(this.position.x, this.position.y, 50, 150)
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
   }
   update(){
     this.draw()
-    this.position.y += 10
+   
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+
+    if (this.position.y + this.height +this.velocity.y >= canvas.height){
+      this.velocity.y = 0
+    } else  this.velocity.y += gravity
+    
   }
 }
 
@@ -27,7 +36,7 @@ class Sprite {
 const player = new Sprite({
   position: {
     x: 0,
-    y: 0
+    y: 10
   },
   velocity: {
     x: 0,
@@ -49,9 +58,46 @@ const enemy = new Sprite({
 enemy.draw()
 
 console.log(player, enemy)
+//more keys:
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
 //create an infinite loop to animate players
 function animate() {
   window.requestAnimationFrame(animate)
-  player
+  c.fillStyle = 'black'
+  c.fillRect(0,0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
 }
 animate()
+
+//moving characters with event listerners + assgning keys
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'd':
+    player.velocity.x = 1
+    break
+    case 'a':
+    player.velocity.x = -1
+    break
+  }
+  console.log(event.key);
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+    player.velocity.x = 0
+    break
+    case 'a':
+    player.velocity.x = 0
+    break
+  }
+  console.log(event.key);
+})
